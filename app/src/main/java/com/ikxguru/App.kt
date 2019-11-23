8675c0a4298bce97b459c9944ce5be6d07052239
ikxguru <ikxguru@gmail.com>
2019-11-23 17:26:48 +0900

    🔥 Remove koin module

--- app/src/main/java/com/ikxguru/App.kt
+++ app/src/main/java/com/ikxguru/App.kt
@@ -3,26 +3,7 @@ package com.ikxguru
 import android.app.Application
 import com.ikxguru.di.component.AppComponent
 import com.ikxguru.di.component.DaggerAppComponent
-import com.ikxguru.di.module.appModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 
 class App : Application() {
 
-    companion object {
-        lateinit var appComponent: AppComponent
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        appComponent = DaggerAppComponent.factory().create(this@App)
-
-        startKoin {
-            androidLogger()
-            androidContext(this@App)
-            modules(appModule)
-        }
-    }
 }
